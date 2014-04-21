a59f277b960572035d780f2ab41144c9b53be04f
Saigut <gosaigut@gmail.com>
2014-04-21 22:08:30 +0800

    Adjust comment.

--- WeeksOfTerm/C/WeeksOfTerm.c
+++ WeeksOfTerm/C/WeeksOfTerm.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[])
   
   struct  tm termbegin = {0, 0, 0, atoi(argv[1]), atoi(argv[2])-1, atoi(argv[3])-1900, 0, 0, 0};
   termbegin_sec = mktime(&termbegin);
-  termbegin = *localtime(&termbegin_sec);		// last two expressions add day of week to termbegin
+  termbegin = *localtime(&termbegin_sec);		// last two expressions add day of week to termbegin.
  
-  cal_diffdays = (int)((now - termbegin_sec)/(60*60*24)) + dayofweek(termbegin.tm_wday);		// cal_diffdays from first week's Monday.
-  weeks = cal_diffdays/7 + ((cal_diffdays%7)?1:0);		// odd out of 7 is a week
+  cal_diffdays = (int)((now - termbegin_sec)/(60*60*24)) + dayofweek(termbegin.tm_wday);		// cal_diffdays starts from first week's Monday.
+  weeks = cal_diffdays/7 + ((cal_diffdays%7)?1:0);		// odd out of 7 regards as a week.
   printf("Today is week %d from term beginning.\n", weeks);
 
   return 0;
@@ -39,5 +39,5 @@ int dayofweek(int day)
 void showusage(void)
 {
   puts("\nUsage: ProgramName dd mm yyyy");
-  puts("\nNotice: This date should be the date of the term's beginning");
+  puts("\nNotice: This date should be the date of the term's beginning.");
 }
